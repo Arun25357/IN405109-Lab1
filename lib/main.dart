016dd5d7e1ccc50cce8ec81de587b599d094afdb
Fitywe <96216623+Arun25357@users.noreply.github.com>
2024-07-12 15:29:07 +0700

    update DetailPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lab1/DetailPage.dart';
+import 'package:lab1/student.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,78 +11,78 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 98, 242, 15)),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: MyAppPage(),
+      home: const MyAppPage(),
     );
   }
 }
-class MyAppPage extends StatelessWidget {
-  @override
-      Widget build(BuildContext context) {
-    List<Map<String, String>> people = [
-      {"name": "อรัญ ศรีสวัสดิ์ 643450095-5", "image": 'image/Arun.jpg'},
-      {"name": "กฤติยา สินโพธิ์ 643450320-4", "image": 'image/B1.jpg'},
-      {"name": "ปรเมศวร์ สิทธิมงคล 643450078-5", "image": 'image/Parames.jpg'},
-      {"name": "พรธิตา ขานพล 643450080-8", "image": 'image/B5.jpg'},
-      {"name": "อมรรัตน์ มาระเหว 643450094-7", "image": 'image/B2.jpg'},
-      {"name": "ณัฐธิดา บุญพา 643450647-2", "image": 'image/B4.jpg'},
-      {"name": "ทัศนีย์ มลาตรี 	643450075-1", "image": 'image/B3.jpg'},
-      {"name": "รัตพงษ์ ปานเจริญ 643450650-3", "image": 'image/Ratpong.jpg'},
-    ];
 
+class MyAppPage extends StatelessWidget {
+  const MyAppPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Student> datas = [
+      Student("อรัญ ศรีสวัสดิ์", "643450095-5", "image/Arun.jpg","arun.s@kkkumail.com","ชอบแก้ไขปัญหา เพราะมักจะมีปัญหาเข้ามาหาอยู่เสมอ","FB: Arun Srisawad"),
+      
+    ];
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("My App"),
       ),
       body: ListView.builder(
-        itemCount: people.length,
-        itemBuilder: (context, index) {
-          return displayNameItem(
-            name: people[index]["name"]!,
-            imagePath: people[index]["image"]!,
-          );
-        },
-      ),
+          itemCount: datas.length,
+          itemBuilder: (context, index) {
+            return displayItem(context: context, student: datas[index]);
+          }),
     );
   }
 
-  Widget displayNameItem({required String name, required String imagePath}) {
-    bool isHighlight = name == "อรัญ ศรีสวัสดิ์ 643450095-5";
-
-    return Container(
-      padding: const EdgeInsets.all(10),
-      color: isHighlight ? Colors.yellow : Colors.transparent,
-      child: Row(
-        children: [
-          Image(
-            width: 100,
-            image: AssetImage(imagePath),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          Text(
-            name,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: isHighlight ? FontWeight.bold : FontWeight.normal,
-              color: isHighlight ? Colors.red : Colors.black,
+  Widget displayItem(
+    {required BuildContext context, required Student student}) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => DetailPage(
+                      data: student,
+                    )));
+      },
+      child: Container(
+          padding: const EdgeInsets.all(10),
+          
+          child: Row(children: [
+            Image(
+              width: 100,
+              image: AssetImage(student.image),
             ),
-          ),
-        ],
-      ),
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  student.name,
+                  style: const TextStyle(fontSize: 20),
+                ),
+                Text(
+                  student.studentid,
+                  style: const TextStyle(fontSize: 12),
+                )
+              ],
+            )
+          ])),
     );
   }
-
 }
-
-
